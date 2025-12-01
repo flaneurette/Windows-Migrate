9ef602ea6d163b4a096d28ca09e8f2c08250f955
Flaneurette <30347642+flaneurette@users.noreply.github.com>
2025-12-01 13:26:24 +0100

    Update UninstallNextDNS.bat

--- Migrate/Install/UninstallNextDNS.bat
+++ Migrate/Install/UninstallNextDNS.bat
@@ -5,9 +5,17 @@ setlocal enabledelayedexpansion
 set /p config=Do you want to remove the installed Next DNS settings? press Y .. 
 if "%config%"=="" goto ask
 
+:askdnsid1
+set /p dnsid1=Enter your NextDNS DNS server 1: 
+if "%dnsid1%"=="" goto askdnsid1
+
+:askdnsid2
+set /p dnsid2=Enter your NextDNS DNS server 2: 
+if "%dnsid2%"=="" goto askdnsid2
+
 echo === Removing DNS settings... ===
-powershell -Command "Remove-DnsClientDohServerAddress -ServerAddress 45.90.30.0"
-powershell -Command "Remove-DnsClientDohServerAddress -ServerAddress 45.90.28.0"
+powershell -Command "Remove-DnsClientDohServerAddress -ServerAddress %dnsid1%"
+powershell -Command "Remove-DnsClientDohServerAddress -ServerAddress %dnsid2%"
 echo Done!
 
 pause
